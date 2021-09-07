ee8ab8d5f9faca96130b55eb6593d1ecf973e80b
shahl12 <90127604+shahl12@users.noreply.github.com>
2021-09-07 13:43:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '👉 OUR CHANNEL👈', url="https://t.me/gameofseriesandmovies"
                                 )
                         ]
                     ]
