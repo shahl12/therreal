fa4f014cdcbd4a922fa26e374122184774cf2187
shahl12 <90127604+shahl12@users.noreply.github.com>
2021-09-07 14:00:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -73,17 +73,17 @@ async def auto_filter(bot: Client, update: Message):
             if file_size < 1024:
                 file_size = f"[{file_size} B]"
             elif file_size < (1024**2):
-                file_size = f"[{str(round(file_size/1024, 2))} KiB] "
+                file_size = f"[{str(round(file_size/1024, 2))} KB] "
             elif file_size < (1024**3):
-                file_size = f"[{str(round(file_size/(1024**2), 2))} MiB] "
+                file_size = f"[{str(round(file_size/(1024**2), 2))} MB] "
             elif file_size < (1024**4):
-                file_size = f"[{str(round(file_size/(1024**3), 2))} GiB] "
+                file_size = f"[{str(round(file_size/(1024**3), 2))} GB] "
             
             
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            button_text = f"{file_size} 🎬{file_name}"
             
             
             if file_type == "video":
@@ -155,7 +155,7 @@ async def auto_filter(bot: Client, update: Message):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"🔰 Page 1🔰/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
         ])
         
         
